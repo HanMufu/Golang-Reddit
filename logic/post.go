296a43aa4abcbf596c43178b331d813dd6dbeecb
hanmufu <hanmufu@gmail.com>
2020-11-19 23:24:40 -0500

    Implemented GetPostDetail API

--- logic/post.go
+++ logic/post.go
@@ -4,6 +4,8 @@ import (
 	"go-web-app/dao/mysql"
 	"go-web-app/models"
 	"go-web-app/pkg/snowflake"
+
+	"go.uber.org/zap"
 )
 
 func CreatePost(p *models.Post) (err error) {
@@ -11,6 +13,26 @@ func CreatePost(p *models.Post) (err error) {
 	return mysql.CreatePost(p)
 }
 
-func GetPostById(pid int64) (data *models.Post, err error) {
-	return mysql.GetPostById(pid)
+func GetPostById(pid int64) (data *models.PostDetail, err error) {
+	post, err := mysql.GetPostById(pid)
+	if err != nil {
+		zap.L().Error("mysql.GetPostById(pid) failed", zap.Int64("pid", pid), zap.Error(err))
+		return
+	}
+	user, err := mysql.GetUserByID(post.AuthorId)
+	if err != nil {
+		zap.L().Error("mysql.GetUserById(post.AuthorId) failed", zap.Int64("author", post.AuthorId), zap.Error(err))
+		return
+	}
+	communityDetail, err := mysql.GetCommunityDetailByID(post.CommunityID)
+	if err != nil {
+		zap.L().Error("mysql.GetCommunityDetailByID(post.CommunityID) failed", zap.Int64("CommunityID", post.CommunityID), zap.Error(err))
+		return
+	}
+	data = &models.PostDetail{
+		AuthorName:      user.Username,
+		Post:            post,
+		CommunityDetail: communityDetail,
+	}
+	return
 }
